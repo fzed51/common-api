7c816170ecf76ad0ebbad672c62f0ed5cd7b40ee
Fabien Sanchez <fzed51+git@gmail.com>
2019-09-26 14:57:35 +0200

    fix | fonction dd

--- function.php
+++ function.php
@@ -1,7 +1,19 @@
 <?php
 
-function dd ($var) {
+function dd($var)
+{
+    $bTrace = debug_backtrace();
+    if (count($bTrace) > 0) {
+        $file = $bTrace[0]['file'];
+        $line = $bTrace[0]['line'];
+        echo $file . ':' . $line . ':' . PHP_EOL;
+    }
+    ob_start();
     var_dump($var);
+    $vd = ob_get_clean();
+    $vd = preg_split("/[\r\n]+/", trim($vd));
+    unset($vd[0]);
+    echo implode (PHP_EOL, $vd) . PHP_EOL;
     die;
 }
 
